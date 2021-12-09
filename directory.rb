4811818d34c9eea95341e4556e89f3d532016ee9
Adam Hindmarsh <adam_hindmarsh@hotmail.com>
2021-12-09 15:22:57 +0000

    load students

--- directory.rb
+++ directory.rb
@@ -47,6 +47,8 @@ def process(selection)
    show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9" 
     exit 
   else 
@@ -80,5 +82,4 @@ def load_students
   file.close
 end 
 
-load_students
 interactive_menu
