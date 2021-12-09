59193867b5eb4d1b0a89e0e90469e9d1357d7e0f
Adam Hindmarsh <adam_hindmarsh@hotmail.com>
2021-12-09 14:50:41 +0000

    refactored

--- directory.rb
+++ directory.rb
@@ -1,50 +1,61 @@
+@students = []
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print_name(students)
-   students.each_with_index do |student, index|
+def print_name
+   @students.each_with_index do |student, index|
     puts "#{(index + 1)}. #{student[:name]}, (#{student[:cohort]} cohort.)"
    end
  end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
-  name = gets.chomp 
+  name = gets.chomp
   while !name.empty? do 
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :December}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
 end
 
+def print_menu 
+  puts "1. Input students"
+  puts "2. Print student list"
+  puts "9. Exit"
+end
+
+def show_students
+  print_name
+  print_header
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+   show_students
+  when "9" 
+    exit 
+  else 
+    puts "I don't understand, try again."
+  end
+end
+
+
 def interactive_menu 
-  students = []
   loop do 
-    puts "1. Input students"
-    puts "2. Print student list"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_name(students)
-      print_header
-      print_footer(students)
-    when "9" 
-      exit 
-    else 
-      puts "I don't understand, try again."
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
